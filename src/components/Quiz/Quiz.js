956d9ef2d55358d8314f463d40fbb2e54d17cd1e
Suzon999 <ashikurrahamansuzon346112.com>
2022-10-12 15:25:33 +0600

    condition added

--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -1,20 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FaEye } from 'react-icons/fa'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import './Quiz.css'
 const Quiz = ({ quiz }) => {
-    const { options, question } = quiz;
-    console.log(options, question);
+    const [isShow, setIsShow] = useState(false)
+    const { options, question, correctAnswer } = quiz;
+    console.log(quiz);
+    const hendleAns = (ans) => {
+        if (correctAnswer === ans) {
+            toast.success("wow !! your answers is right!!!");
+        }
+        else {
+            toast.error("wrong ! your answers is wrong!!!!");
+        }
+    }
+    if (isShow) {
+        toast.success(`${correctAnswer}`);
+    }
     return (
         <div className='quiz-container container mt-5'>
             <div className='eye-icon '>
                 <h3> Quiz :{question} </h3>
-                <p><FaEye></FaEye> </p>
+                <p onClick={() => { setIsShow(true) }}><FaEye></FaEye> </p>
             </div>
             <div className='option'>
                 {
-                    options.map(option => <p>{option} </p>)
+                    options.map(option => <p onClick={() => hendleAns(option)}>{option} </p>)
                 }
             </div>
+            <ToastContainer />
         </div>
     );
 };
