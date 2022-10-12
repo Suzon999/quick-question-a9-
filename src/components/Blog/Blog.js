a660f20a52b76ca059066840b10e8ab5f0d3f8ee
Suzon999 <ashikurrahamansuzon346112.com>
2022-10-12 20:43:53 +0600

    css style added

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -1,38 +1,40 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
 import './Blog.css'
 
 const Blog = () => {
     return (
         <div className='container blog'>
-            <div className='my-5'>
-                <h1>1. what is the purpose of react router?</h1>
-                <p>Routing is a process in which a user is directed to different pages based on their action or request. ReactJS Router is mainly used for developing Single Page Web Applications. React Router is used to define multiple routes in the application. When a user types a specific URL into the browser, and if this URL path matches any 'route' inside the router file, the user will be redirected to that particular route.
+            <h1 className='text-center blog-h1'>Read Your <span className='span-color'>Favorite Blog !!</span></h1>
+            <div className='my-5 blog-1'>
+                <h1 className='span-color'>1. what is the purpose of react router?</h1>
+                <p className='my-5 fs-5'>Routing is a process in which a user is directed to different pages based on their action or request. ReactJS Router is mainly used for developing Single Page Web Applications. React Router is used to define multiple routes in the application. When a user types a specific URL into the browser, and if this URL path matches any 'route' inside the router file, the user will be redirected to that particular route.
 
-                    React Router is a standard library system built on top of the React and used to create routing in the React application using React Router Package. It provides the synchronous URL on the browser with data that will be displayed on the web page. It maintains the standard structure and behavior of the application and mainly used for developing single page web applications.</p>
+                    React Router is a standard library system built on top of the React and used to create routing in the React application using React Router Package. It provides the synchronous URL on the browser with data that will be displayed on the web page. It maintains the standard structure and behavior of the application and mainly used for developing single page web applications.......</p>
+                <Button className='px-5 py-2 fs-4 bg-color text-white' variant="">Read More...</Button>{' '}
             </div>
-            <div className='my-5'>
-                <h1>2. how does context api work?</h1>
-                <p>The React Context API is a way for a React app to effectively produce global variables that can be passed around. This is the alternative to "prop drilling" or moving props from grandparent to child to parent, and so on. Context is also touted as an easier, lighter approach to state management using Redux.
+            <div className='my-5 blog-2'>
+                <h1 className='span-color'>2. how does context api work?</h1>
+                <p className='my-5 fs-5'>The React Context API is a way for a React app to effectively produce global variables that can be passed around. This is the alternative to "prop drilling" or moving props from grandparent to child to parent, and so on. Context is also touted as an easier, lighter approach to state management using Redux.
 
                     Context API is a (kind of) new feature added in version 16.3 of React that allows one to share state across the entire app (or part of it) lightly and with ease.
 
                     React context API: How it works?
-                    React.createContext() is all you need. It returns a consumer and a provider. Provider is a component that as it's names suggests provides the state to its children. It will hold the "store" and be the parent of all the components that might need that store. Consumer as it so happens is a component that consumes and uses the state. More information can be found on React's documentation page</p>
+                    React.createContext() is all you need. It returns a consumer and a provider. Provider is a component that as it's names suggests provides the state to its children. It will hold the "store" and be the parent of all the components that might need that store. Consumer as it so happens is a component that consumes and uses the state. More information can be found on React's documentation page.........</p>
+                <Button className='px-5 py-2 fs-4 bg-color text-white' variant="">Read More....</Button>{' '}
             </div>
-            <div className='my-5'>
-                <h1>3. What is Use Ref?</h1>
-                <p>In the typical React dataflow, props are the only way that parent components interact with their children. To modify a child, you re-render it with new props. However, there are a few cases where you need to imperatively modify a child outside of the typical dataflow. The child to be modified could be an instance of a React component, or it could be a DOM element. For both of these cases, React provides an escape hatch.There are a few good use cases for refs:
+            <div className='my-5 blog-3'>
+                <h1 className='span-color'>3. What is Use Ref?</h1>
+                <p className='my-5 fs-5'>In the typical React dataflow, props are the only way that parent components interact with their children. To modify a child, you re-render it with new props. However, there are a few cases where you need to imperatively modify a child outside of the typical dataflow. The child to be modified could be an instance of a React component, or it could be a DOM element. For both of these cases, React provides an escape hatch.There are a few good use cases for refs:
 
                     Managing focus, text selection, or media playback.
                     Triggering imperative animations.
                     Integrating with third-party DOM libraries.
                     Avoid using refs for anything that can be done declaratively.
 
-                    For example, instead of exposing open() and close() methods on a Dialog component, pass an isOpen prop to it.
-
-
-
+                    For example, instead of exposing open() and close() methods on a Dialog component, pass an isOpen prop to it........
                 </p>
+                <Button className='px-5 py-2 fs-4 bg-color text-white' variant="">Read More....</Button>{' '}
             </div>
         </div>
     );
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -10,17 +10,17 @@ const Header = () => {
     return (
         <Navbar className='navber' bg="light" expand="lg">
             <Container>
-                <Navbar.Brand className='fs-3 fw-bold text-primary' href="#home">Quiz Learning School</Navbar.Brand>
+                <Navbar.Brand className='fs-2 fw-bold text-primary py-2' href="#home">Quiz Learning School</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ms-auto ">
                         {/* <Nav.Link active to= '/topics'>Topics</Nav.Link>
                         <Nav.Link active to="/Statistics">Statistics</Nav.Link>
                         <Nav.Link active to="/Blog">Blog</Nav.Link> */}
-                        <Link className='fs-5 mx-5 text-decoration-none' to='/' >Home</Link>
-                        <Link className='fs-5 text-decoration-none' to='/' >Topics</Link>
-                        <Link className='mx-5 fs-5 text-decoration-none' to='/statistics' >Statistics</Link>
-                        <Link className='fs-5 text-decoration-none' to='/blog' >Blog</Link>
+                        <Link className='fs-4 mx-5 text-decoration-none' to='/' >Home</Link>
+                        <Link className='fs-4 text-decoration-none' to='/' >Topics</Link>
+                        <Link className='mx-5 fs-4 text-decoration-none' to='/statistics' >Statistics</Link>
+                        <Link className='fs-4 text-decoration-none' to='/blog' >Blog</Link>
 
 
                     </Nav>
